f3ea804c71725ca4aecb89a7050aaff87178e430
Crius <crius@crius.ai>
2026-03-18 16:39:07 +0000

    improve(cleanup): remove redundant DisableFlagParsing: false from clone command

--- cmd/clone.go
+++ cmd/clone.go
@@ -38,8 +38,7 @@ Examples:
   gitch clone --hook git@github.com:company/project.git
   gitch clone git@github.com:company/project.git -- --depth 1`,
 	Args:               cobra.MinimumNArgs(1),
-	RunE:               runClone,
-	DisableFlagParsing: false,
+	RunE: runClone,
 }
 
 func init() {
